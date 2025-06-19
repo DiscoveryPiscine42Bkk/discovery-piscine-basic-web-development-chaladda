f7e6a4d10d9952daece656e14eb74de3ecad97cb
POKOMAN <83363927+poko56@users.noreply.github.com>
2025-06-19 22:02:16 +0700

    Rush

--- cell00/ex03/count_files.sh
+++
@@ -1,2 +0,0 @@
-echo "Regular files: $(find . -maxdepth 1 -type f | wc -l)"
-echo "Directories: $(find . -maxdepth 1 -type d | wc -l)"
--- cell00/ex05/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# filepath: build.sh
-
-for arg in "$@"
-do
-    dir="ex$arg"
-    mkdir -p "$dir"
-    echo "Created directory: $dir"
-done
